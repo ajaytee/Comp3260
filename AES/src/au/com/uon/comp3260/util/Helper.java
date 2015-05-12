033f92baf104bf6aa31333ebb79c96f82d31e7e0
Felix Behrendt <Felix@macbook-pro.newcastle.edu.au>
2015-05-12 21:30:35 +1000

    Added more helper methods

--- AES/src/au/com/uon/comp3260/util/Helper.java
+++ AES/src/au/com/uon/comp3260/util/Helper.java
@@ -39,4 +39,53 @@ public class Helper {
 		return readLine(inputFile, 1);
 	}
 
+	public static byte[] stringToByteArray(String binaryString) {
+		byte[] output = new byte[16];
+		for (int i = 0; i < 16; i++) {
+			String part = binaryString.substring(i * 8, (i + 1) * 8);
+			System.out.println("Reading string part: \"" + part + "\" i:" + i);
+			output[i] = Byte.parseByte(part);
+		}
+		return output;
+	}
+
+	public static byte[][] convertArrayToMatrix(byte[] array) {
+		byte[][] hexMatrix = new byte[4][4];
+		int k = 0;
+		for (int i = 0; i < 4; i++) { // i = row #
+			for (int j = 0; j < 4; j++) { // j = column #
+				hexMatrix[j][i] = array[k];
+				k++;
+			}
+		}
+		return hexMatrix;
+	}
+
+	public static byte[][] stringToMatrix(String binaryString) {
+		return convertArrayToMatrix(stringToByteArray(binaryString));
+	}
+
+	public static byte[] convertMatrixToArray(byte[][] matrix) {
+		byte[] array = new byte[16];
+		int k = 0;
+		for (int i = 0; i < 4; i++) { // i = row #
+			for (int j = 0; j < 4; j++) { // j = column #
+				array[k] = matrix[j][i];
+				k++;
+			}
+		}
+		return array;
+	}
+
+	public static String stringForMatrix(byte[][] matrix) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 4; i++) { // i = row #
+			for (int j = 0; j < 4; j++) { // j = column #
+				sb.append(matrix[j][i]);
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
+
 }
