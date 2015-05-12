762c1765b69b4ae2d58f438a160c9c8741a2e843
Felix Behrendt <Felix@macbook-pro.newcastle.edu.au>
2015-05-12 13:54:21 +1000

    add round key (ongoing)

--- AES/src/au/com/uon/comp3260/AddRoundKey.java
+++ AES/src/au/com/uon/comp3260/AddRoundKey.java
@@ -9,4 +9,11 @@ package au.com.uon.comp3260;
  */
 public class AddRoundKey {
 
+	private byte[][] addRoundKey(byte[][] matrix, byte[] key) {
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+				matrix[j][i] ^= key[i * 4 + j];
+		return matrix;
+	}
+
 }
