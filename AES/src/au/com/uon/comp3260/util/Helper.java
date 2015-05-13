4d7948833b8743dff0a6cee8109bbef6e5ea3527
Felix Behrendt <Felix@macbook-pro.newcastle.edu.au>
2015-05-13 14:38:36 +1000

    Added javadoc to Helper functions

--- AES/src/au/com/uon/comp3260/util/Helper.java
+++ AES/src/au/com/uon/comp3260/util/Helper.java
@@ -1,6 +1,5 @@
 package au.com.uon.comp3260.util;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -15,30 +14,73 @@ import java.util.List;
 public class Helper {
 
 	private static String readLine(Path inputFile, int lineNbr) {
+		String line = null;
 		try {
 			List<String> lines = Files.readAllLines(inputFile);
 			if (lines != null && lines.size() > lineNbr) {
-				return lines.get(lineNbr).replaceAll(" ", "");
+				// Remove optional whitespaces that might be in the input file
+				line = lines.get(lineNbr).replaceAll(" ", "");
+				if (line.length() != 128) {
+					throw new Exception(
+							"The textfile must contain 128 0s and 1s!");
+				}
+			} else {
+				throw new Exception("The file does not contain "
+						+ (lineNbr + 1) + " lines");
 			}
-		} catch (IOException e) {
-			System.err.println("Could not read key from text");
+		} catch (Exception e) {
+			line = null;
+			System.err.println("Could not read text from file: "
+					+ e.getMessage());
 			e.printStackTrace();
 		}
-		return null;
+		return line;
 	}
 
+	/**
+	 * Reads a plain text from an input file. The cipher text must have a length
+	 * of 128 characters containing 0s and 1s and whitespaces.
+	 * 
+	 * @param inputFile
+	 *            path to a file
+	 * @return cipher text as a String
+	 */
 	public static String readPlainText(Path inputFile) {
 		return readLine(inputFile, 0);
 	}
 
+	/**
+	 * Reads a cipher text from an input file. The cipher text must have a
+	 * length of 128 characters containing 0s and 1s and whitespaces.
+	 * 
+	 * @param inputFile
+	 *            path to a file
+	 * @return cipher text as a String
+	 */
 	public static String readCipherText(Path inputFile) {
 		return readLine(inputFile, 0);
 	}
 
+	/**
+	 * Reads the key from a given file. The key must be in the second line in
+	 * the file.
+	 * 
+	 * @param inputFile
+	 *            path to a file
+	 * @return key as String
+	 */
 	public static String readKey(Path inputFile) {
 		return readLine(inputFile, 1);
 	}
 
+	/**
+	 * Parses a string to a byte array
+	 * 
+	 * @param binaryString
+	 *            containing 0s and 1s and it must have a length of 128
+	 *            characters
+	 * @return byte array containing the data parsed from the string
+	 */
 	public static byte[] stringToByteArray(String binaryString) {
 		byte[] output = new byte[16];
 		for (int i = 0; i < 16; i++) {
@@ -48,6 +90,13 @@ public class Helper {
 		return output;
 	}
 
+	/**
+	 * Converts a byte array to a 4x4 matrix
+	 * 
+	 * @param array
+	 *            to be converted
+	 * @return byte matrix with data of the array
+	 */
 	public static byte[][] convertArrayToMatrix(byte[] array) {
 		byte[][] hexMatrix = new byte[4][4];
 		int k = 0;
@@ -60,6 +109,13 @@ public class Helper {
 		return hexMatrix;
 	}
 
+	/**
+	 * Converts a byte matrix to a byte array
+	 * 
+	 * @param matrix
+	 *            to be converted
+	 * @return byte array with the data of the matix
+	 */
 	public static byte[] convertMatrixToArray(byte[][] matrix) {
 		byte[] array = new byte[16];
 		int k = 0;
@@ -72,32 +128,62 @@ public class Helper {
 		return array;
 	}
 
+	/**
+	 * Formats a byte matrix to a string. Each row in the matrix will be
+	 * presented by a single line in the string.
+	 * 
+	 * @param matrix
+	 *            to be converted
+	 * @param pretty
+	 *            if true, a whitespace will be added after each 8 bits.
+	 * @return String representing the matix
+	 */
 	public static String matrixToString(byte[][] matrix, boolean pretty) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 4; i++) { // i = row #
 			for (int j = 0; j < 4; j++) { // j = column #
 				Byte b = matrix[j][i];
-				sb.append(stringForByte(b) + " ");
+				sb.append(byteToString(b) + " ");
 				if (pretty)
 					sb.append(" ");
 			}
-			sb.append("\n");
+			if (pretty)
+				sb.append("\n");
 		}
 		return sb.toString();
 	}
 
+	/**
+	 * Formats a byte array to a string. each byte will be presented by 8
+	 * characters. 2 Arrays of the same length will always have the same output
+	 * length
+	 * 
+	 * @param array
+	 *            input array
+	 * @param pretty
+	 *            if true, a whitespace will be inserted after each byte
+	 * @return String
+	 */
 	public static String arrayToString(byte[] array, boolean pretty) {
 		StringBuilder sb = new StringBuilder();
 		for (int j = 0; j < 16; j++) {
 			Byte b = array[j];
-			sb.append(stringForByte(b));
+			sb.append(byteToString(b));
 			if (pretty)
 				sb.append(" ");
 		}
 		return sb.toString();
 	}
 
-	public static String stringForByte(byte b) {
+	/**
+	 * Formats a byte to a string with a length of 8. If the byte is smaller
+	 * than 128 (=7 bytes) the string will be padded with leading 0s.
+	 * 
+	 * @param b
+	 *            byte to be formatted
+	 * @return output string with a length of 8
+	 */
+	public static String byteToString(byte b) {
 		String value = Integer.toBinaryString(Byte.toUnsignedInt(b));
 		int missingZeros = 8 - value.length();
 		for (int i = 0; i < missingZeros; i++) {
