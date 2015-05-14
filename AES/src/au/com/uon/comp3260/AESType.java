de8302a058f772fb41b260abeb91ead34ab9f7f5
Felix Behrendt <Felix@macbook-pro.newcastle.edu.au>
2015-05-14 22:53:19 +1000

    AESType now has a name to simplify debugging

--- AES/src/au/com/uon/comp3260/AESType.java
+++ AES/src/au/com/uon/comp3260/AESType.java
@@ -5,23 +5,29 @@ package au.com.uon.comp3260;
  * @author Felix Behrendt
  */
 public enum AESType {
-	AES0(false, false, false, false),
-	/**/AES1(true, false, false, false),
-	/**/AES2(false, true, false, false),
-	/**/AES3(false, false, true, false),
-	/**/AES4(false, false, false, true);
+	AES0("AES0", false, false, false, false),
+	/**/AES1("AES1", true, false, false, false),
+	/**/AES2("AES2", false, true, false, false),
+	/**/AES3("AES3", false, false, true, false),
+	/**/AES4("AES4", false, false, false, true);
 
+	private String name;
 	private boolean skipSubBytes;
 	private boolean skipShiftRows;
 	private boolean skipMixColumns;
 	private boolean skipAddRoundKey;
 
-	private AESType(boolean skipSubBytes, boolean skipShiftRows,
+	private AESType(String name, boolean skipSubBytes, boolean skipShiftRows,
 			boolean skipMixColumns, boolean skipAddRoundKey) {
+		this.name = name;
 		this.skipSubBytes = skipSubBytes;
 		this.skipShiftRows = skipShiftRows;
 		this.skipMixColumns = skipMixColumns;
 		this.skipAddRoundKey = skipAddRoundKey;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	public boolean isSkipSubBytes() {
