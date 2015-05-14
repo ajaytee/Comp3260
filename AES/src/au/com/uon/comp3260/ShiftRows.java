bbab152b2ea86767a132074c39205b2f59d29107
Felix Behrendt <Felix@macbook-pro.newcastle.edu.au>
2015-05-14 12:51:12 +1000

    Bug fix in ShiftRows > instead of <

--- AES/src/au/com/uon/comp3260/ShiftRows.java
+++ AES/src/au/com/uon/comp3260/ShiftRows.java
@@ -8,29 +8,29 @@ package au.com.uon.comp3260;
  */
 public class ShiftRows {
 
-	
 	public byte[][] shiftRows(byte[][] input, boolean decrypt) {
-		
+
 		byte[][] output = new byte[4][4];
-		
+
 		if (!decrypt) {
 			// shift left - encryption
-			for (int i = 1; i < 4; i++) {			// initialise i to 1 as Row 0 remains unchanged
-				for (int j = 0; j < 4; j++) {		// i = rows, j = columns
+			for (int i = 1; i < 4; i++) { // initialise i to 1 as Row 0 remains
+											// unchanged
+				for (int j = 0; j < 4; j++) { // i = rows, j = columns
 					int newColumn = j - i;
 					if (newColumn < 0) {
-						newColumn = newColumn + 4;	// circular shift
+						newColumn = newColumn + 4; // circular shift
 					}
 					output[i][newColumn] = input[i][j];
 				}
 			}
-			
+
 		} else {
 			// shift right - decryption
 			for (int i = 1; i < 4; i++) {
 				for (int j = 0; j < 4; j++) {
 					int newColumn = j + i;
-					if (newColumn < 3) {
+					if (newColumn > 3) {
 						newColumn = newColumn - 4;
 					}
 					output[i][newColumn] = input[i][j];
